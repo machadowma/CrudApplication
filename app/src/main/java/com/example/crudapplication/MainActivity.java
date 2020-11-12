91c74850a35cefc33bf8c68ecf077775a3562904
wagner <machado.wma@gmail.com>
2020-11-12 10:55:25 -0300

    Método Excluir

--- app/src/main/java/com/example/crudapplication/MainActivity.java
+++ app/src/main/java/com/example/crudapplication/MainActivity.java
@@ -8,9 +8,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase bancoDados;
     public ListView listViewDados;
     public Button botao;
+    public ArrayList<Integer> arrayIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 abrirTelaCadastro();
+            }
+        });
+
+        listViewDados.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                excluir(i);
+                return true;
             }
         });
 
@@ -59,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void listarDados(){
         try {
+            arrayIds = new ArrayList<>();
             bancoDados = openOrCreateDatabase("crudapp", MODE_PRIVATE, null);
             Cursor meuCursor = bancoDados.rawQuery("SELECT id, nome FROM coisa", null);
             ArrayList<String> linhas = new ArrayList<String>();
@@ -72,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
             meuCursor.moveToFirst();
             while(meuCursor!=null){
                 linhas.add(meuCursor.getString(1));
+                arrayIds.add(meuCursor.getInt(0));
                 meuCursor.moveToNext();
             }
 
@@ -104,5 +117,20 @@ public class MainActivity extends AppCompatActivity {
     public void abrirTelaCadastro(){
         Intent intent = new Intent(this,CadastroActivity.class);
         startActivity(intent);
+    }
+
+    public void excluir(Integer i){
+        //Toast.makeText(this, i.toString(), Toast.LENGTH_SHORT).show();
+        try{
+            bancoDados = openOrCreateDatabase("crudapp", MODE_PRIVATE, null);
+            String sql = "DELETE FROM coisa WHERE id =?";
+            SQLiteStatement stmt = bancoDados.compileStatement(sql);
+            stmt.bindLong(1, arrayIds.get(i));
+            stmt.executeUpdateDelete();
+            listarDados();
+            bancoDados.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
